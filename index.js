f17cf97fa3b21b4c24bf65305afe8419ad0eaf9a
Marwa Sh <marwa.al.shahal@gmail.com>
2020-12-16 13:44:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your Circle class here
+class circle{
+  constructor (radius){
+    this.radius=radius;
+  }
+  get diameter(){
+     return this.radius*2;
+  }
+  get circumference(){
+    return Math.PI*this.radius*2;
+  }
+  get area(){
+    return MATH.PI*this.radius**2
+  }
+}
