489ce62f0b13e59f521d3018540748bccdcaca5a
Marwa Sh <marwa.al.shahal@gmail.com>
2020-12-16 13:55:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ class circle {
      return this.radius*2;
   }
   get circumference(){
-    return Math.PI*this.radius*2;
+    return Math.PI * this.radius * 2;
   }
   get area(){
-    return MATH.PI*this.radius**2;
+    return MATH.PI * this.radius * this.radius;
   }
-  set diameter(){
-    return this.radius*2;
+  set diameter(diameter){
+     this.diameter=this.radius*2;
+  }
+  set circumference(circumference){
+     this.circumference=circumference;
+  }
+  set area(area){
+    this.area=area;
   }
 }
